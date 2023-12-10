8e00f263cc4fc7f9453590d9b238e4a8e4b31f45
DaviSM <davimesquita@gmail.com>
2023-12-10 19:53:13 -0300

    exibindo algo

--- prog.js
+++ prog.js
@@ -22,23 +22,113 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-const url = "https://wchat.mundial.workers.dev";
-
-function teste() {
-    fetch(url+"/test", {
+function user(who) {
+    fetch("/user", {
         method: "POST",
-        mode: "no-cors",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "name": "John Doe",
-            "age": 30
-        })
+            user: who,
+        }),
     }).then(function (response) {
         return response.json();
-    }).then(
-        function (data) {
-        alert(JSON.stringify(data));
+    }).then(function (data) {
+
+        let html = "";
+
+        if (data && data.chat && data.chat.length > 0) {
+            for (const message of data.chat) {
+                if (message.role === "user") {
+
+                    html += `
+                    <!-- Chat: left -->
+                    <li class="mb-3 d-flex flex-row align-items-end">
+                        <div class="max-width-70">
+                            <div class="user-info mb-1">
+                                 <div class="avatar rounded-circle no-thumbnail">WH</div>
+                                <span class="text-muted small">${data.whatsappUser}</span>
+                            </div>
+                            <div class="card border-0 p-3">
+                                <div class="message">${message.content}</div>
+                            </div>
+                        </div>
+                    </li>`
+
+                } else if (message.role === "assistant") {
+
+                    html +=
+                        `<!-- Chat: right -->
+                        <li class="mb-3 d-flex flex-row-reverse align-items-end">
+                            <div class="max-width-70 text-end">
+                                <div class="user-info mb-1">
+                                    <span class="text-muted small">${data.role}</span>
+                                </div>
+                                <div class="card border-0 p-3 bg-primary text-light">
+                                    <div class="message">${message.content}</div>
+                                </div>
+                            </div>
+                        </li>`
+
+                }
+            }
+        }
+
+
+        $("#chatPanel").html(html);
+
+    });
+}
+
+function next() {
+    fetch("/next", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        let html = "";
+        for (const key in data) {
+
+            let user = data[key];
+
+            html +=
+                `<li class="list-group-item px-md-4 py-2 py-md-3">
+                    <a href="javascript:user('${key}')" class="d-flex">
+                        <div class="avatar rounded-circle no-thumbnail">WH</div>
+                        <div class="flex-fill ms-3 text-truncate">
+                            <h6 class="d-flex justify-content-between mb-0"><span>${user.nome}</span> <small class="msg-time small">${user.fechado ? 'Fechado' : 'Aberto'}</small></h6>
+                            <span class="text-muted small">${user.fromNow}</span>
+                        </div>
+                    </a>
+                </li>`;
+
+        }
+
+        $("#chatList").html(html);
+
+        setTimeout(() => {
+
+                next();
+
+            },
+            1000
+        );
+
+    });
+}
+
+function init() {
+    $(function () {
+
+        setTimeout(() => {
+
+                next();
+
+            },
+            1000
+        );
     });
 }
